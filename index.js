301ae85eb0ccdb4c5afe16646a885f10502a4219
vignesh <vigneshfornavy@gmail.com>
2023-11-10 22:39:36 +0530

    final setup

--- index.js
+++ index.js
@@ -2,31 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
-
-app.set("view enginer","ejs");
+const { v4: uuidv4 } = require('uuid');
+app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")))
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 app.use(express.urlencoded({extended:true}));
 
 let posts = [
     {
-        id:1,
+        id: uuidv4(),
         username : "vignesh",
-        content : "hey am vignesh",
-        profile : ""
+        content : "hey am vignesh dsffdsafafs",
     },
     {
-        id:2,
+        id:uuidv4(),
         username : "vignesh",
         content : "hey am vignesh",
-        profile : ""
     }
     ,    {
-        id:3,
+        id:uuidv4(),
         username : "vignesh",
         content : "hey am vignesh",
-        profile : ""
     }
 ]
 
@@ -38,7 +37,7 @@ app.listen(port,()=>{
 
 app.get("/",(req,res)=>{
     res.render("home.ejs",{posts});
-})
+})  
 
 
 app.get("/posts",(req,res)=>{
@@ -48,7 +47,38 @@ app.get("/posts",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let {username,content,profile} = req.body;
-    posts.push({username,content,profile});
+    let id = uuidv4();
+    posts.push({id,username,content});
     res.redirect("/");
 
+})
+
+
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("post",{post});
+})
+
+
+app.delete("/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/");
+})
+
+
+app.get("/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit",{post});
+})
+
+app.patch("/:id",(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    console.log(id);
+    post.content = newContent
+    res.render("/");
 })
